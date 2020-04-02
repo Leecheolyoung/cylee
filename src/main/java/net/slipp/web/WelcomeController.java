82545b5a78e4a732b7a347861a8101b04d2bd3af
jeremy0625 <lcy806@gmail.com>
2020-04-02 16:34:59 +0900

    test
    Change-Id: I6000359da9dc940d220aec690c0976aaced0b8f2

--- src/main/java/net/slipp/web/WelcomeController.java
+++ src/main/java/net/slipp/web/WelcomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
-	@GetMapping("/helloworld")
+	@GetMapping("/nexacroTest")
 	/*
 	public String welcome(String name, int age, Model model) {
 		System.out.println("name : "+name+"age :"+ age);
@@ -23,6 +23,6 @@ public class WelcomeController {
 		List<MyModel> repo = Arrays.asList(new MyModel("cylee1"), new MyModel("cylee2"), new MyModel("cylee3"));
 		model.addAttribute("repo",repo);
 		
-		return "welcome";
+		return "nexacroTest";
 	}
 }
